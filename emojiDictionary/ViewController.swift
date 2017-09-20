8760aa401cd0c99dfff7ddc7a432f57e1815bdda
German Sanchez Tlachi <gstlachi@mactlachi.local>
2017-09-20 11:06:51 -0500

    Initial Commit

---
+++ emojiDictionary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  emojiDictionary
+//
+//  Created by German Sanchez Tlachi on 20/09/17.
+//  Copyright © 2017 gstlachiApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
